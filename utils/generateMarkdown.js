6828b39033e4fe18003030573a9466fe30b4fd29
LB Vik <laura.bullek@gmail.com>
2021-05-08 20:59:50 -0400

    Created the function to generate markdown for the README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -19,17 +19,63 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license[0]) {
+    case 'MIT':
+      return '(https://opensource.org/licenses/MIT)'
+      
+    case 'ISC':
+      return '(https://opensource.org/licenses/ISC)'
+      
+    case 'MPL 2.0':
+      return '(https://opensource.org/licenses/MPL-2.0)'
+      
+    case 'GNU GPL v3':
+      return '(https://www.gnu.org/licenses/gpl-3.0)'
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  function renderLicenseSection(license) {
+    return renderLicenseBadge(license) + renderLicenseLink(license)
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  ## Badges
+  ${renderLicenseSection(data.license)}
+  
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions) 
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Credits
+  ${data.credits}
+  ## License
+  ${data.license}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  https://github.com/${data.githubUsername}
+  ${data.emailAddress}
+  
+  `;
 }
 
 module.exports = generateMarkdown;
